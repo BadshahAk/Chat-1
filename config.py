a9d78d89a75992cad6642e1f76fc05580f984d6c
⫷ Aɳαɳԃ Sԋαɾɱα ⫸ <91930171+BadshahAk@users.noreply.github.com>
2025-04-19 21:27:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "29695886"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "14ca35d1ae7a732d32f2f28c83ed54db"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "7995626429"))
-SUPPORT_GRP = "THUNDER_SUPPORT_ll"
-UPDATE_CHNL = "THUNDERDEVS"
-OWNER_USERNAME = "ll_ISTKHAR_BABY_lll"
+OWNER_ID = int(getenv("OWNER_ID", "7291963092"))
+SUPPORT_GRP = "Unique_society"
+UPDATE_CHNL = "New_sanatani"
+OWNER_USERNAME = "GAYATRIxANAND"
